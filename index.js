6c16dfe091348e0513ef2926f40771ffb2465583
Mimi Shih <meitsens@andrew.cmu.edu>
2023-05-14 01:22:33 -0400

    fix responsive style

---
+++ index.js
@@ -0,0 +1,7 @@
+// $('.flip-card-back').click(function() {
+//     $('.flip-card').css({'pointer-events': 'none'});
+// });
+
+// $('.flip-card-front').click(function() {
+//     $('.flip-card').css({'pointer-events': 'fill'});
+// });
