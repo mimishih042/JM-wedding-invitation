570b6b8a680186db4ceb092c5cf00cbbd3efa830
Mimi Shih <meitsens@andrew.cmu.edu>
2023-05-14 17:16:44 -0400

    fix hover state

--- index.js
+++
@@ -1,7 +0,0 @@
-// $('.flip-card-back').click(function() {
-//     $('.flip-card').css({'pointer-events': 'none'});
-// });
-
-// $('.flip-card-front').click(function() {
-//     $('.flip-card').css({'pointer-events': 'fill'});
-// });
